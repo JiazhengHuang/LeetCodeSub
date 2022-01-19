45f44fba5fd455f3a933913f1f726b0b0c9c8e7a
JiazhengHuang <44761814+JiazhengHuang@users.noreply.github.com>
2022-01-18 18:50:30 -0800

    Time: 377 ms (20.77%), Space: 106.8 MB (12.14%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -1,25 +1,36 @@
 class Solution {
     public String longestPalindrome(String s) {
-        if (s == null || s.length() < 1) return "";
-        int start = 0, end = 0;
-        for (int i = 0; i < s.length(); i++) {
-            int len1 = expandAroundCenter(s, i, i);
-            int len2 = expandAroundCenter(s, i, i + 1);
-            int len = Math.max(len1, len2);
-            if (len > end - start) {
-                start = i - (len - 1) / 2;
-                end = i + len / 2;
+        if (s == null || s.length() == 0) {
+            return "";
+        }
+        
+        int n = s.length();
+        boolean[][] isPalindrome = new boolean[n][n];
+        
+        int longest = 1, start = 0;
+        for (int i = 0; i < n; i++) {
+            isPalindrome[i][i] = true;
+        }
+        for (int i = 0; i < n - 1; i++) {
+            isPalindrome[i][i + 1] = s.charAt(i) == s.charAt(i + 1);
+            if (isPalindrome[i][i + 1]) {
+                start = i;
+                longest = 2;
             }
         }
-        return s.substring(start, end + 1);
-    }
-
-    private int expandAroundCenter(String s, int left, int right) {
-        int L = left, R = right;
-        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
-            L--;
-            R++;
+        
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i + 2; j < n; j++) {
+                isPalindrome[i][j] = isPalindrome[i + 1][j - 1] && 
+                    s.charAt(i) == s.charAt(j);
+                    
+                if (isPalindrome[i][j] && j - i + 1 > longest) {
+                    start = i;
+                    longest = j - i + 1;
+                }
+            }
         }
-        return R - L - 1;
+        
+        return s.substring(start, start + longest);
     }
 }
